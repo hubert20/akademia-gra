e0afa2bb3173e2dd572bd61451cd0f3946f91890
hubert20 <hubert@tomedia.pl>
2025-09-01 19:34:17 +0200

    Style fix

--- src/js/views/view16.js
+++ src/js/views/view16.js
@@ -13,54 +13,61 @@ import dymekMain from '../../assets/task6/dymek-main.png';
 
 
 export const view = `
-<div class="fact-info d-flex gap-4 p-3 mt-4" style="background: #E3F5FA;border-radius: 1rem;">
-<div class="fact-info__left" style="position: relative;top: -20px;">
-  <img src="${iconInfo}" alt="Postać"  class="" style="" />
-  <p class="mb-0" style="color: #006FCC;font-weight: bold;font-size: 20px;">Ciekawostka</p>
-</div>
-<div class="p-3 text-start" style="font-size: 20px;color: #006FCC;line-height: 1.2;">
-   Asertywność to umiejętność mówienia tego, co naprawdę myślimy i czujemy - z szacunkiem do siebie i innych. 
- Nie chodzi o bycie miłym za wszelką cenę - ale o to, by stawiać granice i dbać o siebie.
-</div>
-</div>
-  <div class="assertive-task">
-    <div class="row gx-5 gy-4 align-items-start">
-      <div class="col-md-3 text-center">
-        <img src="${characterNeutral}" alt="Postać" id="character" class="character-img" style="max-height: 260px;" />
-      </div>
-      <div class="col-md-9">
-        <div class="question-box p-4">
-          <div class="question-header mb-3 d-flex align-items-center justify-content-center gap-2">
-            <img src="${iconFlaga}" alt="Ikona flagi" style="height: 28px;" />
-            <h4 class="m-0">Które zdania są asertywne?</h4>
-          </div>
-         <div class="px-5 pb-5">
-          <div class="options-wrapper row g-1 justify-content-center">
-           <div class="col-5 pe-3 pb-3">
-             <button class="option-btn" data-id="1">„Nie muszę mieć ochoty robić tego co robią inni.”</button>
-            </div>
-            <div class="col-5 pb-3">
-             <button class="option-btn" data-id="2">„Zawsze muszę zgodzić się z tym co sądzi mój najlepszy przyjaciel.”</button>
-            </div>
-            <div class="col-5 pe-3">
-             <button class="option-btn" data-id="3">„Mogę nie zgodzić się z opinią moich bliskich.”</button>
-            </div>
-            <div class="col-5">
-             <button class="option-btn" data-id="4">„Moje zdanie jest mniej ważne od zdania innych.”</button>
-            </div>
-          </div>
-         </div>
-         <div class="row">
-          <div class="col-lg-9">
-           <div id="feedback" class="alert alert-danger d-none">
-            <p class="m-0" id="feedbackText"></p>
-          </div>
-         </div>
-         </div>
+<div class="task-6">
+    <div class="fact-info d-flex gap-4 p-3 mt-4" style="background: #E3F5FA;border-radius: 1rem;">
+        <div class="fact-info__left" style="position: relative;top: -20px;">
+            <img src="${iconInfo}" alt="Postać" class="" style="" />
+            <p class="mb-0" style="color: #006FCC;font-weight: bold;font-size: 20px;">Ciekawostka</p>
         </div>
-      </div>
+        <div class="p-3 text-start" style="font-size: 20px;color: #006FCC;line-height: 1.2;">
+            Asertywność to umiejętność mówienia tego, co naprawdę myślimy i czujemy - z szacunkiem do siebie i innych.
+            Nie chodzi o bycie miłym za wszelką cenę - ale o to, by stawiać granice i dbać o siebie.
+        </div>
     </div>
-  </div>
+    <div class="assertive-task">
+        <div class="row gx-5 gy-4 align-items-start">
+            <div class="col-md-3 text-center">
+                <img src="${characterNeutral}" alt="Postać" id="character" class="character-img"
+                    style="max-height: 260px;" />
+            </div>
+            <div class="col-md-9">
+                <div class="question-box p-4">
+                    <div class="question-header mb-3 d-flex align-items-center justify-content-center gap-2">
+                        <img src="${iconFlaga}" alt="Ikona flagi" style="height: 28px;" />
+                        <h4 class="m-0">Które zdania są asertywne?</h4>
+                    </div>
+                    <div class="px-5 pb-5">
+                        <div class="options-wrapper row g-1 justify-content-center">
+                            <div class="col-5 pe-3 pb-3">
+                                <button class="option-btn" data-id="1">„Nie muszę mieć ochoty robić tego co robią
+                                    inni.”</button>
+                            </div>
+                            <div class="col-5 pb-3">
+                                <button class="option-btn" data-id="2">„Zawsze muszę zgodzić się z tym co sądzi mój
+                                    najlepszy przyjaciel.”</button>
+                            </div>
+                            <div class="col-5 pe-3">
+                                <button class="option-btn" data-id="3">„Mogę nie zgodzić się z opinią moich
+                                    bliskich.”</button>
+                            </div>
+                            <div class="col-5">
+                                <button class="option-btn" data-id="4">„Moje zdanie jest mniej ważne od zdania
+                                    innych.”</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-9">
+                            <div id="feedback" class="alert alert-danger d-none">
+                                <p class="m-0" id="feedbackText"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 `;
 
 export const logicFunc = (onSuccess) => {
